d9201038e1397ca947b064bae35be9e2bdbadaaf
ermek-bdnf <67462589+ermek-bdnf@users.noreply.github.com>
2020-08-11 08:09:02 +0300

    Update try_catch.cpp

--- try_catch.cpp
+++ try_catch.cpp
@@ -1,28 +1,23 @@
 #include<iostream>
 #include<fstream>
-using namespace std;
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	//try ... catch
-	
 	ifstream a;
 	a.exceptions(ifstream::badbit | ifstream::failbit);
-	
 	try
 	{
-		cout << "Trying to open a file\n";
+		std::cout << "Trying to open a file\n";
 		a.open("text.txt"); //basic_ios::clear
 		//a.open("src.txt"); //The file was opened successfully
-		cout << "The file was opened successfully\n";
+		std::cout << "The file was opened successfully\n";
 		a.close();
 	}
-	catch(const exception& e)
+	catch(const std::exception& e)
 	{
-		cout << e.what() << endl; //basic_ios::clear
+		std::cout << e.what() << std::endl; //basic_ios::clear
 	}
-	
 	return 0;
-
 }
-
